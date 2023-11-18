3d1aed7c2537070fb8da55c26a8c9ccc8cc8c67c
Ben Allen <ben@benallen.dev>
2023-11-18 19:22:16 +0100

    DRY out the basic HTML page wrapper, make all the different root views

--- cmd/main.go
+++ cmd/main.go
@@ -14,6 +14,14 @@ func main() {
 		views.Index().Render(r.Context(), w)
 	})
 
+	http.HandleFunc("/presenter", func(w http.ResponseWriter, r *http.Request) {
+		views.Presenter().Render(r.Context(), w)
+	})
+
+	http.HandleFunc("/participant", func(w http.ResponseWriter, r *http.Request) {
+		views.Participant().Render(r.Context(), w)
+	})
+
 	http.HandleFunc("/name", func(w http.ResponseWriter, r *http.Request) {
 		views.Hello(util.GetRandomName()).Render(r.Context(), w)
 	})
